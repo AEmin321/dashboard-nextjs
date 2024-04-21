80f1383d1ae337bda521255c2e2047afcf918426
hamini <ahmademin321@gmail.com>
2024-04-22 02:50:17 +0300

    Add: add new product page with styles

---
+++ app/dashboard/products/add/page.jsx
@@ -0,0 +1,40 @@
+import styles from "@/app/ui/dashboard/products/addProduct/addProduct.module.css";
+import Image from "next/image";
+
+const AddProduct = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.productContainer}>
+        <Image
+          className={styles.productImg}
+          src="/product.jpg"
+          height={128}
+          width={128}
+          alt="place holder image of a product."
+        />
+      </div>
+      <form className={styles.form} action="#">
+        <input type="text" placeholder="Title" name="title" required />
+        <input type="text" placeholder="Size" name="size" />
+        <textarea
+          name="description"
+          id="description"
+          cols="30"
+          rows="10"
+          placeholder="Description"
+        ></textarea>
+        <input type="text" placeholder="Color" name="color" />
+        <select name="category" id="category">
+          <option value="choose">Choose a Category</option>
+          <option value="sun hats">Sun Hats</option>
+          <option value="winter hats">Winter Hats</option>
+        </select>
+        <input type="number" placeholder="Price" name="price" />
+        <input type="number" placeholder="Stock" name="stock" />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default AddProduct;
