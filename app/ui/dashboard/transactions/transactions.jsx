bf6ddb3b25dd07642e2488c2cd302c9e792d7b76
hamini <ahmademin321@gmail.com>
2024-04-21 04:39:08 +0300

    Add: transaction table component with styles

--- app/ui/dashboard/transactions/transactions.jsx
+++ app/ui/dashboard/transactions/transactions.jsx
@@ -1,7 +1,87 @@
 import React from "react";
+import styles from "./transactions.module.css";
 
 const Transactions = () => {
-  return <div>Transactions</div>;
+  return (
+    <div className={styles.container}>
+      <h3 className={styles.title}>Recent Sales</h3>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Name</td>
+            <td>Date</td>
+            <td>Amount</td>
+            <td>Status</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <div className={styles.user}>
+                <img
+                  className={styles.userImage}
+                  src="/profile.jpg"
+                  width={40}
+                  height={40}
+                  alt="profile photo"
+                />
+                Elif
+              </div>
+            </td>
+            <td>12/12/2024</td>
+            <td>2200₺</td>
+            <td>
+              <span className={`${styles.status} ${styles.pending}`}>
+                Pending
+              </span>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <div className={styles.user}>
+                <img
+                  className={styles.userImage}
+                  src="/profile.jpg"
+                  width={40}
+                  height={40}
+                  alt="profile photo"
+                />
+                Elif
+              </div>
+            </td>
+            <td>12/12/2024</td>
+            <td>2200₺</td>
+            <td>
+              <span className={`${styles.status} ${styles.completed}`}>
+                Completed
+              </span>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <div className={styles.user}>
+                <img
+                  className={styles.userImage}
+                  src="/profile.jpg"
+                  width={40}
+                  height={40}
+                  alt="profile photo"
+                />
+                Elif
+              </div>
+            </td>
+            <td>12/12/2024</td>
+            <td>2200₺</td>
+            <td>
+              <span className={`${styles.status} ${styles.cancelled}`}>
+                Cancelled
+              </span>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default Transactions;
