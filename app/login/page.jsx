fa2f5825d85ee6792a95f45620b693ceb6b3edf9
hamini <ahmademin321@gmail.com>
2024-04-22 17:21:23 +0300

    Add: login page with it's styles and layout

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import styles from "@/app/ui/login/login.module.css";
+import { MdLogin } from "react-icons/md";
 
 const page = () => {
-  return <div>login page.</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <div className={styles.logo}>
+          <h3>MeWowDash.</h3>
+        </div>
+        <form action="" className={styles.form}>
+          <div className={styles.loginTxt}>Login</div>
+          <input type="text" name="username" placeholder="Username" />
+          <input type="password" name="password" placeholder="Password" />
+          <button className={styles.loginBtn}>
+            <MdLogin />
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default page;
