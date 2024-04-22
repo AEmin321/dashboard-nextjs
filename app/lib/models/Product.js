8c00b967a39a8138084547d43fc5e200f20153f5
hamini <ahmademin321@gmail.com>
2024-04-22 19:24:56 +0300

    Add: mongodb connection and data model for user and product

---
+++ app/lib/models/Product.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      min: 5,
+      max: 50,
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
+    size: {
+      type: Number,
+    },
+    color: {
+      type: String,
+    },
+    stock: {
+      type: Number,
+      min: 0,
+    },
+    img: {
+      type: String,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Product = mongoose.model("Product", productSchema);
+export default Product;
---
+++ app/lib/models/User.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      min: 3,
+      max: 20,
+    },
+    phone: {
+      type: String,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    address: {
+      type: String,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    img: {
+      type: String,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+export default User;
